d9d857a6b4b32c810563c3b1ca2dd943956d1b45
LimaGustav <limagustavo100@hotmail.com>
2026-06-17 11:46:11 -0300

    🔨 Containerize application with Docker and add environment configuration

--- docker/mysql/init/01-init-schemas-and-users.sh
+++ docker/mysql/init/01-init-schemas-and-users.sh
@@ -15,18 +15,22 @@ CREATE DATABASE IF NOT EXISTS ledger
   CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS prizes
   CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS daily_chest
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER IF NOT EXISTS 'auth_svc'@'%' IDENTIFIED WITH mysql_native_password BY '${AUTH_DB_PASSWORD}';
 CREATE USER IF NOT EXISTS 'profile_svc'@'%' IDENTIFIED WITH mysql_native_password BY '${PROFILE_DB_PASSWORD}';
 CREATE USER IF NOT EXISTS 'campaigns_svc'@'%' IDENTIFIED WITH mysql_native_password BY '${CAMPAIGNS_DB_PASSWORD}';
 CREATE USER IF NOT EXISTS 'ledger_svc'@'%' IDENTIFIED WITH mysql_native_password BY '${LEDGER_DB_PASSWORD}';
 CREATE USER IF NOT EXISTS 'prizes_svc'@'%' IDENTIFIED WITH mysql_native_password BY '${PRIZES_DB_PASSWORD}';
+CREATE USER IF NOT EXISTS 'daily_chest_svc'@'%' IDENTIFIED WITH mysql_native_password BY '${DAILY_CHEST_DB_PASSWORD}';
 
 GRANT ALL PRIVILEGES ON auth.* TO 'auth_svc'@'%';
 GRANT ALL PRIVILEGES ON profile.* TO 'profile_svc'@'%';
 GRANT ALL PRIVILEGES ON campaigns.* TO 'campaigns_svc'@'%';
 GRANT ALL PRIVILEGES ON ledger.* TO 'ledger_svc'@'%';
 GRANT ALL PRIVILEGES ON prizes.* TO 'prizes_svc'@'%';
+GRANT ALL PRIVILEGES ON daily_chest.* TO 'daily_chest_svc'@'%';
 
 FLUSH PRIVILEGES;
 SQL
